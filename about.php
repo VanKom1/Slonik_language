767ea664c0eb21f1059379d5bc3aeae55c7befc8
Ivan Komlik <ikomlik32@gmail.com>
2022-09-09 15:09:55 +0300

    menu site

---
+++ about.php
@@ -0,0 +1,30 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head> 
+
+<pre>
+<?php
+
+$mas = [
+    "main" => ["title" => "Главная","file" => "main.php"],
+    "about" => ["title" => "О нас","file" => "about.php"],
+    "blog" => ["title" => "Статьи","file" => "blog.php"],
+    "acount" => ["title" => "Акаунт","file" => "acount.php"]
+];
+echo '<header>';
+
+echo "<ul>";
+foreach($mas as $key => $val){
+    if($key != "main" && $key != "main"){
+        echo '<li>|<a href = "' . $val["file"] .'">'.$val["title"].'</a></li>';
+    }else{
+        echo '<li><a href = "' . $val["file"] .'">'.$val["title"].'</a></li>';
+    }
+}
+unset($val);
+echo "</ul>";
+echo '</header>';
+
+echo'вы на странице "О нас"';
+
+?>
---
+++ main.php
@@ -0,0 +1,29 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head> 
+
+<pre>
+<?php
+
+$mas = [
+    "main" => ["title" => "Главная","file" => "main.php"],
+    "about" => ["title" => "О нас","file" => "about.php"],
+    "blog" => ["title" => "Статьи","file" => "blog.php"],
+    "acount" => ["title" => "Акаунт","file" => "acount.php"]
+];
+echo '<header>';
+echo "<ul>";
+foreach($mas as $key => $val){
+    if($key != "main"){
+        echo '<li>|<a href = "' . $val["file"] .'">'.$val["title"].'</a></li>';
+    }else{
+        echo '<li><a href = "' . $val["file"] .'">'.$val["title"].'</a></li>';
+    }
+}
+unset($val);
+echo "</ul>";
+echo '</header>';
+
+echo'вы на странице "Главная"';
+
+?>
